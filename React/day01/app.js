198a4f31e7ebdc5b9e62fc6b653405bdc6f0e48c
rohit_639 <rohit391174@gmail.com>
2026-01-05 09:46:01 +0530

    Update React

--- React/day01/app.js
+++ React/day01/app.js
@@ -73,6 +73,13 @@
 // }
 
 
+// const ReactDOM = {
+//     render: function(child, parent){
+//         parent.append(child);
+//     }
+// }
+
+
 // Original React
 
 
@@ -90,36 +97,65 @@ const React = {
 }
 
 
-const reactElement = {
-    type:'h1',
-    props:{
-        className:"element",
-        id:"first",
-        style:{fontSize:"30px",backgroundColor:"orange",color:"white"},
-        children: "Hello Coder Army"
+// const reactElement = {
+//     type:'h1',
+//     props:{
+//         className:"element",
+//         id:"first",
+//         style:{fontSize:"30px",backgroundColor:"orange",color:"white"},
+//         children: "Hello Coder Army"
+//     }
+// }
+
+const ReactDOM = {
+    render: function(reactElement,root){
+       
+       root.innerHTML = '';
+
+       const element =  document.createElement(reactElement.type);
+        
+      const {props} = reactElement;
+
+      for(const key in props){
+        if(key==='style'){
+            Object.assign(element.style,props.style);
+        }
+        else if(key==='children'){
+            element.textContent = props[key];
+        }
+        else{
+            element[key] = props[key];
+        }
+      }
+
+      root.append(element);
     }
 }
 
-
-
-// const ReactDOM = {
-//     render: function(child, parent){
-//         parent.append(child);
-//     }
-// }
 const element1 = React.createElement("h1",{className:"element",id:"first",style:{fontSize:"30px",backgroundColor:"red",color:"white"}},"Hello Coder Army");
 
 // Now same if i want to craete  new h2 element
 const element2 = React.createElement("h1",{className:"element",id:"second",style:{fontSize:"30px",backgroundColor:"green",color:"white"}},"Strike is launched");
 
 
+
+console.log(element1);
+ReactDOM.render(element1,document.getElementById('root'));
+ReactDOM.render(element2,document.getElementById('root'));
+
+
+
+
+
+
+
 // Now next if i want to create h3 element simple just call the function 
 
-const root=document.getElementById('root');
+// const root=document.getElementById('root');
 // root.append(element1);
 // root.append(element2);  //this can be done using ReactDOM.render=> 
-ReactDOM.render(element1,root);
-ReactDOM.render(element2,root);
+// ReactDOM.render(element1,root);
+// ReactDOM.render(element2,root);
 
  // ReactDOM.render(element1,document.getElementById('root'));
  // ReactDOM.render(element2,document.getElementById('root'));
@@ -133,4 +169,9 @@ ReactDOM.render(element2,root);
     // Now → React does this work using ReactDOM.render()
 
 
-// React ka kaam hai sirf is javascript object ya react element ko create karna.Dom ke sath kiska koi kaamnhi hai yhap appko Dom ka koi operation nhi dikhega(This is the work of React DOM).React only tells how your ui should look element creation work is done by React DOM
+
+
+// React’s job is only to create JavaScript objects (React elements).
+// React does not work directly with the DOM and performs no DOM operations.
+// All DOM-related work is handled by ReactDOM.
+// React only tells how the UI should look, and the actual element creation and rendering in the DOM is done by ReactDOM.
